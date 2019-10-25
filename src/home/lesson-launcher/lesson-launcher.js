2769b3a7373b3326e1123d33224f1e0caf69c495
Rokas <rokas.anisas@gmail.com>
2019-10-25 15:50:09 +0300

    remove unnecessary js

--- src/home/lesson-launcher/lesson-launcher.js
+++ src/home/lesson-launcher/lesson-launcher.js
@@ -1,14 +1,4 @@
 const lessonLauncher = () => {
-    const buttonLauchers = document.getElementsByClassName('launcher-button__launcher')
-
-    for (var i = 0; i < buttonLauchers.length; i++) {
-        buttonLauchers[i].addEventListener('click', (e) => {
-            const selectedLesson = e.currentTarget.dataset.launcher
-            const lessonView = document.getElementById(`lesson-view-${selectedLesson}`)
-
-        })
-    }
-
     window.onhashchange = function () {
         setActiveLesson()
     }
@@ -18,13 +8,16 @@ const lessonLauncher = () => {
         const allLessonViews = document.getElementsByClassName('lesson-launcher__content')
         const activeLesson = document.getElementById(`lesson-view-${currentLesson}`)
 
+        console.log(allLessonViews)
+
+
         for (var i = 0; i < allLessonViews.length; i++) {
             allLessonViews[i].classList.add('-closed')
         }
+
         if (activeLesson) {
             activeLesson.classList.remove('-closed')
         }
-        console.log(currentLesson)
     }
 
     setActiveLesson()
