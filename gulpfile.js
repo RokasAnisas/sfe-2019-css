6d5e3dcff7a73c019bff1e6feed8747d1d6304b0
Rokas <rokas.anisas@gmail.com>
2019-10-25 10:42:31 +0300

    Gulp builds and serves properly

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const { src, dest, parallel, series } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const DIST_DIR = 'dist';
+const SRC_DIR = 'src';
 
 const gulpClean = require('gulp-clean');
 const cssConcat = require('gulp-concat-css');
 const csso = require('gulp-csso');
+const browserSync = require('browser-sync').create();
 
 function clean() {
     return src(DIST_DIR)
@@ -11,13 +13,35 @@ function clean() {
 }
 
 function css() {
-    return src(['home/*.css','LESSONS/**/*.css'])
+    return src([`${SRC_DIR}/home/*.css`, `${SRC_DIR}/LESSONS/**/*.css`])
         .pipe(cssConcat('styles.min.css'))
         .pipe(csso())
         .pipe(dest(DIST_DIR))
 }
 
+function html() {
+    return src(`${SRC_DIR}/home/index.html`)
+        .pipe(dest(DIST_DIR))
+}
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+
+    watch(SRC_DIR).on('change', build);
+    watch(DIST_DIR).on('change', browserSync.reload);
+}
+
+
 exports.css = css;
 exports.clean = clean;
-const build = parallel(css);
+exports.serve = serve;
+exports.html = html;
+
+const build = parallel(html, css);
+
+exports.dev = series(clean, build, serve);
 exports.default = series(clean, build);
