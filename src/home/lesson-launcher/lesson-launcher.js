a63637a07be095dd3c5ad66a73637cfb95837407
Rokas <rokas.anisas@gmail.com>
2019-10-25 13:40:20 +0300

    Lesson view opening mechanics

--- src/home/lesson-launcher/lesson-launcher.js
+++ src/home/lesson-launcher/lesson-launcher.js
@@ -1,5 +1,36 @@
-const james = () => {
-    console.log("JAMES!");
+const lessonLauncher = () => {
+    const buttonLauchers = document.getElementsByClassName('launcher-button__launcher')
+
+    for (var i = 0; i < buttonLauchers.length; i++) {
+        buttonLauchers[i].addEventListener('click', (e) => {
+            const selectedLesson = e.currentTarget.dataset.launcher
+            const lessonView = document.getElementById(`lesson-view-${selectedLesson}`)
+
+        })
+    }
+
+    window.onhashchange = function () {
+        setActiveLesson()
+    }
+
+    const setActiveLesson = () => {
+        const currentLesson = location.hash.replace('#/lesson-', "")
+        const allLessonViews = document.getElementsByClassName('lesson-launcher__content')
+        const activeLesson = document.getElementById(`lesson-view-${currentLesson}`)
+
+        console.log(allLessonViews)
+
+        for (var i = 0; i < allLessonViews.length; i++) {
+            allLessonViews[i].classList.add('-closed')
+        }
+        if (activeLesson) {
+            activeLesson.classList.remove('-closed')
+        }
+        console.log(currentLesson)
+    }
+
+    setActiveLesson()
+
 }
 
-james();
+lessonLauncher();
