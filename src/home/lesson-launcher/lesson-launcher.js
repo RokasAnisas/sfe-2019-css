e32529954ce275e718cd539b7739a6e90cf0443b
Rokas <rokas.anisas@gmail.com>
2019-10-28 10:26:10 +0200

    lesson launcher classname

--- src/home/lesson-launcher/lesson-launcher.js
+++ src/home/lesson-launcher/lesson-launcher.js
@@ -5,7 +5,7 @@ const lessonLauncher = () => {
 
     const setActiveLesson = () => {
         const currentLesson = location.hash.replace('#/lesson-', "")
-        const allLessonViews = document.getElementsByClassName('lesson-launcher__content')
+        const allLessonViews = document.getElementsByClassName('app-lesson-launcher__content')
         const activeLesson = document.getElementById(`lesson-view-${currentLesson}`)
 
         Array.prototype.forEach.call(allLessonViews, function(lessonView) {
